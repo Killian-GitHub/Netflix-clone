e6bc098100e269a071d1a0c51dfa57c593504681
Killian <mkillian.development@gmail.com>
2021-10-30 23:32:28 +0200

    add row file, axios requests and dotenv config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from 'react'
+import './App.css'
+import Row from './components/Row'
+import requests from './utils/requests'
+
+require('dotenv').config()
 
 function App() {
+
   return (
-   <div>
-     
-   </div>
+    <div className="App">
+      <Row
+        title='NETFLIX ORIGINALS'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title='Les plus gros succès sur Netflix' fetchUrl={requests.fetchTopRated} />
+      <Row title='Tendances actuelles' fetchUrl={requests.fetchTrending} />
+      <Row title='Comédies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Action et aventures' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Romantique' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Docummentaires' fetchUrl={requests.fetchDocumentaries} />
+    </div>
   );
 }
 
